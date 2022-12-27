472da84b813e5f5c18a9a2a7f54179f8d5a5bdd1
ilham <imily.22@live.be>
2022-12-27 09:17:32 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { POKEMONS } from './mock-pokemon-list';
+import { Pokemon } from './pokemon';
 
 @Component({
   selector: 'app-root',
-  template: `<h1>Welcome to {{title}}!</h1>`
+  templateUrl: 'app.component.html'
 })
-export class AppComponent {
-  title = 'ng-pokemon-app';
+export class AppComponent  implements OnInit{
+  pokemonList : Pokemon[] = POKEMONS;
+  pokemonSelected : Pokemon |undefined;
+
+  ngOnInit(): void{ 
+    console.table(this.pokemonList);
+    
+  }
+
+  selectPokemon(pokemonId : string){
+    const pokemon : Pokemon | undefined = this.pokemonList.find(pokemon => pokemon.id == +pokemonId)
+    if(pokemon){
+      console.log(`vous avez demandé le pokemon ${pokemon.name}`);
+      this.pokemonSelected = pokemon;
+    }else{
+      console.log(`vous avez demandé un pokemon qui n'exsite pas.`);
+      this.pokemonSelected = pokemon;
+    }
+    
+  }
+
 }
