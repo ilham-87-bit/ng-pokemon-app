9cdb3435fd757cae3f16494d66831497a97f1324
ilham <imily.22@live.be>
2022-12-29 10:01:16 +0100

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,30 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import { POKEMONS } from './mock-pokemon-list';
-import { Pokemon } from './pokemon';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html'
 })
-export class AppComponent  implements OnInit{
-  pokemonList : Pokemon[] = POKEMONS;
-  pokemonSelected : Pokemon |undefined;
-
-  ngOnInit(): void{ 
-    console.table(this.pokemonList);
-    
-  }
-
-  selectPokemon(pokemonId : string){
-    const pokemon : Pokemon | undefined = this.pokemonList.find(pokemon => pokemon.id == +pokemonId)
-    if(pokemon){
-      console.log(`vous avez demandé le pokemon ${pokemon.name}`);
-      this.pokemonSelected = pokemon;
-    }else{
-      console.log(`vous avez demandé un pokemon qui n'exsite pas.`);
-      this.pokemonSelected = pokemon;
-    }
-    
-  }
-
-}
+export class AppComponent{}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,22 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BorderCardDirective } from './border-card.directive';
-import { PokemonTypeColorPipe } from './pokemon-type-color.pipe';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { PokemonModule } from './pokemon/pokemon.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BorderCardDirective,
-    PokemonTypeColorPipe
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    PokemonModule,
     AppRoutingModule
   ],
   providers: [],
